3b94c715e3a7934674a12ee5a98dc613d7a3f9ae
KaustubhQ <kaustubh.devlop@gmail.com>
2025-10-03 21:57:57 +0530

    |making first function program and initilize the candidate

--- voting_System/lib.rs
+++ voting_System/lib.rs
@@ -18,3 +18,10 @@ pub mod voting {
         ctx.accounts.poll_account.poll_voting_end = end_time;
         Ok(())
     }
+     pub fn initialize_candidate(ctx: Context<InitializeCandidate>, 
+                                _poll_id: u64, 
+                                candidate: String) -> Result<()> {
+        ctx.accounts.candidate_account.candidate_name = candidate;
+        ctx.accounts.poll_account.poll_option_index += 1;
+        Ok(())
+    }
